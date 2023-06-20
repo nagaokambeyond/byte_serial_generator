3756a9b1b92be47ae31c610147a7995ec28c9c56
nagaokambeyond <40172017+nagaokambeyond@users.noreply.github.com>
2023-06-21 01:26:17 +0900

    リファクタリング
    clang-format
    配列の要素数取得方法を変更
    int tail = std::size(arr) - 1;
    ↓
    int tail = (sizeof(arr) / sizeof(short)) - 1;

--- src/main.cpp
+++ src/main.cpp
@@ -1,65 +1,50 @@
-#include <iostream>
 #include <iomanip>
+#include <iostream>
 
 const short MAX_VAL = 255;
 short arr[] = {0, 0, 0, 0};
 
-void display()
-{
-    for (short val : arr)
-    {
+void display() {
+    for (short val : arr) {
         std::cout << std::hex << std::setw(2) << std::setfill('0') << val;
     }
     std::cout << std::endl;
 }
 
-void carry(int idx)
-{
-    if (arr[idx] <= MAX_VAL)
-    {
+void carry(int idx) {
+    if (arr[idx] <= MAX_VAL) {
         return;
     }
     int next_idx = idx - 1;
-    if (next_idx >= 0)
-    {
+    if (next_idx >= 0) {
         arr[idx] = 0;
         arr[next_idx] += 1;
         carry(next_idx);
-    }
-    else
-    {
+    } else {
         display();
     }
 }
 
-bool is_end()
-{
-    for (short val : arr)
-    {
-        if (val != MAX_VAL)
-        {
+bool is_end() {
+    for (short val : arr) {
+        if (val != MAX_VAL) {
             return false;
         }
     }
     return true;
 }
-int main()
-{
-    int tail = std::size(arr) - 1;
-    for (int idx = 0; idx <= tail; idx += 1)
-    {
+int main() {
+    int tail = (sizeof(arr) / sizeof(short)) - 1;
+    for (int idx = 0; idx <= tail; idx += 1) {
         arr[idx] = 0;
     }
 
-    do
-    {
-        for (short val = 0; val <= MAX_VAL; val += 1)
-        {
+    do {
+        for (short val = 0; val <= MAX_VAL; val += 1) {
             arr[tail] = val;
             // display();
         }
-        if (is_end())
-        {
+        if (is_end()) {
             break;
         }
         arr[tail] += 1;
