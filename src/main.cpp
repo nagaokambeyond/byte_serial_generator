3d2e767afbdf46d385159ac9a258ba58378680d2
nagaokambeyond <40172017+nagaokambeyond@users.noreply.github.com>
2023-07-04 22:34:53 +0900

    リファクタリング
    .at()をやめる
    const autoを積極的に使う

--- src/main.cpp
+++ src/main.cpp
@@ -15,15 +15,15 @@ void display() {
 }
 
 void carry(const int idx) {
-    if (arr.at(idx) <= MAX_VAL) {
+    if (arr[idx] <= MAX_VAL) {
         return;
     }
     const int next_idx = idx - 1;
     if (next_idx < 0) {
         return;
     }
-    arr.at(idx) = 0;
-    arr.at(next_idx) += 1;
+    arr[idx] = 0;
+    arr[next_idx] += 1;
     carry(next_idx);
 }
 
@@ -31,26 +31,26 @@ bool is_end() {
     return std::all_of(arr.begin(), arr.end(), [](short val) { return val == MAX_VAL; });
 }
 int main() {
-    std::chrono::system_clock::time_point start = std::chrono::system_clock::now();
+    const auto start = std::chrono::system_clock::now();
 
     std::fill(arr.begin(), arr.end(), 0);
     const int tail = arr.size() - 1;
 
     do {
         for (short val = 0; val <= MAX_VAL; val += 1) {
-            arr.at(tail) = val;
+            arr[tail] = val;
             // display();
         }
         if (is_end()) {
             break;
         }
-        arr.at(tail) += 1;
+        arr[tail] += 1;
         carry(tail);
     } while (!is_end());
     display();
 
-    std::chrono::system_clock::time_point end = std::chrono::system_clock::now();
-    std::chrono::microseconds dur = end - start;
-    long long msec = std::chrono::duration_cast<std::chrono::milliseconds>(dur).count();
+    const auto end = std::chrono::system_clock::now();
+    const auto dur = end - start;
+    const auto msec = std::chrono::duration_cast<std::chrono::milliseconds>(dur).count();
     std::cout << std::dec << msec << " milli sec \n";
 }
